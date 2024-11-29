542e38ed7e40c24fb5a88c962fe379146eb93d7e
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2024-11-29 13:16:21 +0000

    current date display added to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Form from "./Form";
+import { useState } from "react";
 
-function App() {
+function App({ date }) {
+  const [newDate, setNewDate] = useState(new Date());
+
+  date = newDate.toLocaleString(undefined, { 
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
+  });
+    
   return (
     <div>
       <Form />
+      <p>{ date }</p>
     </div>
   );
 }
