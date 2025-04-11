11bfeae79999d4f3a1c2fc80dc40f95d63b131b2
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-04-11 13:08:28 +0100

    theme moved to separate file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import Form from "./Form";
 import { ThemeProvider } from "styled-components";
-
-const theme = {
-  colors: {
-    primaryColor: "teal",
-    actionColor: "#01aaaa",
-  },
-  breakpoints: {
-    mobile: 767,
-  },
-};
+import { theme } from './theme.js'
 
 function App() {
   return (
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    primaryColor: "teal",
+    actionColor: "#01aaaa",
+  },
+  breakpoints: {
+    mobile: 767,
+  },
+};
