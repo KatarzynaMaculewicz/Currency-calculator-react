92872d146247e4bf5d25ed7c906070c59e12c8b5
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-09-09 22:36:39 +0100

    currencies.js has been exchanged for temporary file currencies.json

--- src/currencies.js
+++
@@ -1,7 +0,0 @@
-export const currencies = [
-  { id: 0, name: "", rate: 0.0 },
-  { id: 1, name: "GBP", rate: 1 },
-  { id: 2, name: "EUR", rate: 1.19 },
-  { id: 3, name: "USD", rate: 1.3 },
-  { id: 4, name: "PLN", rate: 5.13 },
-];
