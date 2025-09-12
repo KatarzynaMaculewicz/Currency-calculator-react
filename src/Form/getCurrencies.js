5d5852df6208643c1b066669ac4199dbbc785d9a
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-09-12 12:22:58 +0100

    Remove JSON file and update API URL

--- src/Form/getCurrencies.js
+++ src/Form/getCurrencies.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getCurrencies() {
   try {
-    const response = await axios.get("/currencies.json");
+    const response = await axios.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_2kgH8LVC7c6OYrmDxqL8ZL1HmdncEFslSZ7XjgSA&currencies=GBP%2CEUR%2CUSD%2CPLN%2CCAD%2CJPY%2CAUD%2CCHF%2CNZD%2CRUB%2CMXN%2CZAR%2CSEK%2CNOK%2CDKK%2CCZK%2CRON%2CBGN%2CHRK%2CTRY%2CSGD&base_currency=GBP");
 
     const data = response.data.data;
 
